eba30aea69187779a6092b262d8b909aee2aaedf
Eric-Bwr <boewer2003@gmail.com>
2023-04-20 22:57:20 +0200

    Dep

--- Engine/Camera3D/Camera.h
+++ Engine/Camera3D/Camera.h
@@ -8,7 +8,7 @@ public:
     inline void setMouseSpeed(float mouseSpeed) { this->mouseSpeed = mouseSpeed; }
     inline void setPitch(float pitchMin, float pitchMax) { this->pitchMin = pitchMin; this->pitchMax = pitchMax; }
     inline Mat4d getViewMatrixd() { return viewMatrix; }
-    inline Mat4f getViewMatrixf() { return toFloat(viewMatrix); }
+    inline Mat4f getViewMatrixf() { return convert<float>(viewMatrix); }
 public:
     float mouseSpeed = 0.05;
     float pitchMin = -89.0f, pitchMax = 89.0f;
